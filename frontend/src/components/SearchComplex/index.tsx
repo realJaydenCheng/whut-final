1281655af538915611435e22b7492a6d8b148890
JaydenCheng <jaydencheng@whut.edu.cn>
2024-04-20 09:54:06 +0800

    fix default select value with const, connect search feature

--- frontend/src/components/SearchComplex/index.tsx
+++ frontend/src/components/SearchComplex/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Col, Input, Row, Select } from "antd";
+import { Col, Form, FormInstance, Input, Row, Select } from "antd";
 import StandardFormRow from "./StandardFormRow";
 import TagSelect from "./TagSelect";
 import { useRequest } from "@umijs/max";
@@ -7,16 +7,21 @@ import { getDbDetailApiDbDetailGet } from "@/services/ant-design-pro/getDbDetail
 import FormItem from "antd/es/form/FormItem";
 import { ProFormSelect, ProFormText } from "@ant-design/pro-components";
 import { listDbApiDbListGet } from "@/services/ant-design-pro/listDbApiDbListGet";
+import { from } from "form-data";
+
+type tEvent = React.ChangeEvent<HTMLInputElement> |
+    React.MouseEvent<HTMLElement> |
+    React.KeyboardEvent<HTMLInputElement> | undefined;
+
+type tInfo = { source?: 'clear' | 'input'; } | undefined
 
 interface SearchComplexProps {
-    onSearch: (
-        value: string,
-        event?: React.ChangeEvent<HTMLInputElement> |
-            React.MouseEvent<HTMLElement> |
-            React.KeyboardEvent<HTMLInputElement>,
-        info?: {
-            source?: 'clear' | 'input';
-        }
+    onSearchAndSubmit: (
+        searchValue: string,
+        formData: API.SearchRequest,
+        formInstance: FormInstance<any>,
+        event?: tEvent,
+        info?: tInfo,
     ) => void;
     databaseMetas: API.DatabaseMetaOutput[]
 }
@@ -56,7 +61,7 @@ const SearchComplex: React.FC<SearchComplexProps> = (props) => {
             const categoryOptions = detail?.cate_fields_detail[field_name];
             nodes.push(
                 <StandardFormRow key={field_name} title={field_name} block style={{ padding: 0, marginTop: 0, marginBottom: 3 }}>
-                    <FormItem name={field_name} style={{ padding: 0, marginTop: 0, marginBottom: 3 }}>
+                    <FormItem name={`cateField-${field_name}`} style={{ padding: 0, marginTop: 0, marginBottom: 3 }}>
                         <TagSelect expandable>
                             {categoryOptions.map((category: string) => (
                                 <TagSelect.Option value={category} key={category}>
@@ -70,14 +75,12 @@ const SearchComplex: React.FC<SearchComplexProps> = (props) => {
         }
         setCateFieldsTagSelects(nodes);
     };
-    const renderOtherFieldsText = (detail: API.DatabaseMetaDetail) => {
-        const fields = Array.from(
-            new Set(detail?.id_fields.concat(detail?.text_fields))
-        );
 
-        const nodes = fields?.map((field_name) => {
+    const renderOtherFieldsText = (detail: API.DatabaseMetaDetail) => {
+
+        const genNode = (fieldCate: string, field_name: string) => {
             return <FormItem
-                name={field_name}
+                name={`${fieldCate}-${field_name}`}
                 label={field_name}
                 style={{ marginBottom: 3 }}
                 // see: https://sentry.io/answers/unique-key-prop/
@@ -85,24 +88,78 @@ const SearchComplex: React.FC<SearchComplexProps> = (props) => {
             >
                 <Input size="small" style={{ width: '80%' }} />
             </FormItem>
+        }
 
-        }) || [];
+        let nodes: React.ReactNode[] = [];
+
+        nodes = nodes.concat(detail?.id_fields.map((field_name) => {
+            return genNode("idField", field_name);
+        }));
+        nodes = nodes.concat(detail?.text_fields.map((field_name) => {
+            return genNode("textField", field_name);
+        }));
+
         setOtherFieldsTexts(nodes);
     };
 
+    const [form] = Form.useForm();
+    const pairs = new Map<string, string>(Object.entries({
+        "cateField": "filters",
+        "idField": "sub_terms",
+        "textField": "sub_terms",
+    }));
+    const extractCombinedToNestedObj = (
+        prefixTargetPairs: Map<string, string>,
+        data: object,
+    ) => {
+        const res: any = {};
+        for (const [key, value] of Object.entries(data)) {
+            // Split key into prefix and realKey using the '-' delimiter
+            const [prefix, realKey] = key.split('-');
+            // Find the corresponding target based on the prefix
+            const target = prefixTargetPairs.get(prefix);
+            if (target != undefined) {
+                // If the target does not exist in res, initialize it as an empty object
+                if (!res[target]) {
+                    res[target] = {};
+                }
+                // Assign the value to the correct target and realKey
+                if (value instanceof String) {
+                    res[target][realKey] = value.split(" ");
+                } else if (value instanceof Array) {
+                    res[target][realKey] = value;
+                }
+                
+            }
+        }
+        return res;
+    };
+    const searchToSubmit = (value: string, event: tEvent, info: tInfo) => {
+        const formData = form.getFieldsValue(true);
+            const nestedDate = extractCombinedToNestedObj(pairs, formData)
+            const searchRequest: API.SearchRequest = {
+                terms: (formData["terms"] as string).split(" "),
+                db_id: formData["db_id"],
+                date_range: null,  //FIXME: implement data range.
+                filters: nestedDate["filters"],
+                sub_terms: nestedDate["sub_terms"],
+            }
+        props.onSearchAndSubmit(value, searchRequest, form, event, info);
+    };
+
     return (
-        <>
+        <Form form={form}>
             <div style={{ textAlign: 'center' }}>
                 <Row>
 
                     <Col span={5} offset={5}>
-                        <FormItem name="db_id" initialValue={selectedDbId}>
-                            <Select
+                        <FormItem name="db_id" initialValue='65e94e64-e526-4298-981b-8168eb142605'>
+                            <Select  // FIXME: need to add dynamic default values.
+                                // initValue and defaultValue are not working.
                                 size="large"
                                 value={selectedDbId}
                                 options={selectOptions}
                                 onChange={handleDbSelectChange}
-                            // FIXME: need to add default values.
                             />
                         </FormItem>
                     </Col>
@@ -113,7 +170,7 @@ const SearchComplex: React.FC<SearchComplexProps> = (props) => {
                                 placeholder="请输入"
                                 enterButton="搜索"
                                 size="large"
-                                onSearch={props.onSearch}
+                                onSearch={searchToSubmit}
                                 style={{ width: '90%' }}
                             />
                         </FormItem>
@@ -126,7 +183,7 @@ const SearchComplex: React.FC<SearchComplexProps> = (props) => {
 
             <Row>{otherFieldsTexts}</Row>
 
-        </>
+        </Form>
 
     );
 };
--- frontend/src/pages/Search/index.tsx
+++ frontend/src/pages/Search/index.tsx
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import SearchComplex from "@/components/SearchComplex";
 import { listDbApiDbListGet } from "@/services/ant-design-pro/listDbApiDbListGet";
 import { useRequest } from "@umijs/max";
+import { getSearchResultApiSearchPost } from "@/services/ant-design-pro/getSearchResultApiSearchPost";
 
 const SearchPage: React.FC<{}> = () => {
 
     const [dbMetas, setDbMetas] = useState<API.DatabaseMetaOutput[]>([]);
+
     useRequest(listDbApiDbListGet,
         {
             onSuccess: (data) => {
-                setDbMetas(data||[]);
+                setDbMetas(data || []);
             }
         }
     );
 
+    const {run: postForm} = useRequest(
+        (searchRequest: API.SearchRequest) => getSearchResultApiSearchPost(searchRequest),
+        {
+            manual: true,
+        }
+    )
+
     return <SearchComplex
-        onSearch={() => { }}
+        onSearchAndSubmit={(value, formData, form, event, info) => {
+            postForm(formData);
+        }}
         databaseMetas={dbMetas}
     />;
 };
